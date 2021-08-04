fd1bd3a231fcb1b639b6c8812e93b71853968137
Sagar Parajuli <official.sagar@yahoo.com>
2021-08-04 18:45:28 +1000

    ENV Management

--- Backend/index.js
+++ Backend/index.js
@@ -3,8 +3,9 @@ import mongoose from "mongoose";
 import postRoutes from "./routes/posts.js";
 import cors from "cors";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
 const app = express();
-const PORT = 5000;
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -15,8 +16,8 @@ const DB_CONNECTION =
   "mongodb+srv://sagarparajuli02:hellboy02@cluster0.q3hks.mongodb.net/PostDatabase?retryWrites=true&w=majority";
 
 mongoose
-  .connect(DB_CONNECTION, { useNewUrlParser: true })
+  .connect(process.env.DB_CONNECTION, { useNewUrlParser: true })
   .then(console.log("connected"))
   .catch((err) => console.log(err.message));
 
-app.listen(PORT);
+app.listen(process.env.PORT);
